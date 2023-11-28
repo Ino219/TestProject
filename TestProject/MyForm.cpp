199f77b9ec7640aad3f3b04e691ece17e0718791
井上綾人 <chacha-ayato@infoseek.jp>
2023-11-28 23:03:48 +0900

    プロジェクト ファイルを追加します。

---
+++ TestProject/MyForm.cpp
@@ -0,0 +1,113 @@
+#include "MyForm.h"
+
+using namespace TestProject;
+using namespace System::Collections::Generic;
+using namespace System::IO;
+
+[STAThreadAttribute]
+
+int main() {
+	Application::Run(gcnew MyForm());
+	return 0;
+}
+
+System::Void TestProject::MyForm::button1_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	openFileDialog(1);
+	return System::Void();
+}
+
+System::Void TestProject::MyForm::button2_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	cli::array<String^>^ tmparray = gcnew cli::array<String^>{textBox1->Text,textBox2->Text};
+	cli::array<List<String^>^>^ arrayList = gcnew  cli::array<List<String^>^>(2);
+
+	for (int j = 0; j < tmparray->Length; j++) {
+		String^ fullPath = tmparray[j];
+		String^ Dext = getDeleteExtension(fullPath);
+		String^ Dtail = getDeleteTail(Dext);
+
+		//ディレクトリ名の取得
+		String^ DirName = System::IO::Path::GetDirectoryName(fullPath);
+		cli::array<String^>^ allFile = Directory::GetFiles(DirName);
+		List<String^>^ arr = gcnew List<String^>;
+
+		for (int i = 0; i < allFile->Length; i++) {
+			//ファイル名取得
+			String^ fileName = allFile[i];
+			String^ f_Dext = getDeleteExtension(fileName);
+			String^ f_Dtail = getDeleteTail(f_Dext);
+			//改名後ファイル名の取得
+			if (f_Dtail == Dtail) {
+				arr->Add(fileName);
+			}
+		}
+		if (arr->Count != 0) {
+			arrayList[j] = arr;
+		}
+	}
+
+
+	return System::Void();
+}
+
+System::Void TestProject::MyForm::openFileDialog(int index)
+{
+	OpenFileDialog^ ofd = gcnew OpenFileDialog();
+
+	//はじめのファイル名を指定する
+	//はじめに「ファイル名」で表示される文字列を指定する
+	ofd->FileName = "default.html";
+	//はじめに表示されるフォルダを指定する
+	//指定しない（空の文字列）の時は、現在のディレクトリが表示される
+	ofd->InitialDirectory = "C:\\Users\\chach\\Desktop\\ffolder";
+	//[ファイルの種類]に表示される選択肢を指定する
+	//指定しないとすべてのファイルが表示される
+	ofd->Filter = "HTMLファイル(*.html;*.htm)|*.html;*.htm|すべてのファイル(*.*)|*.*";
+	//[ファイルの種類]ではじめに選択されるものを指定する
+	//2番目の「すべてのファイル」が選択されているようにする
+	ofd->FilterIndex = 2;
+	//タイトルを設定する
+	ofd->Title = "開くファイルを選択してください";
+
+	//ダイアログを表示する
+	if (ofd->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+	{
+		//OKボタンがクリックされたとき、選択されたファイル名を表示する
+		//MessageBox::Show(ofd->FileName);
+		if (index == 1) {
+			textBox1->Text = ofd->FileName;
+		}
+		else if (index == 2) {
+			textBox2->Text = ofd->FileName;
+		}
+	}
+	return System::Void();
+}
+
+String ^ TestProject::MyForm::getDeleteExtension(String ^ str)
+{
+	String^ res = str;
+	cli::array<String^>^ tmpArray = res->Split('.');
+	String^ ext = tmpArray[tmpArray->Length - 1];
+	res = res->Replace("."+ext, "");
+	return res;
+}
+
+String ^ TestProject::MyForm::getDeleteTail(String ^ str)
+{
+	String^ res = str;
+	cli::array<String^>^ tmpArray = res->Split('_');
+	String^ tail = tmpArray[tmpArray->Length - 1];
+	res = res->Replace("_" + tail, "");
+	
+	return res;
+}
+
+System::Void TestProject::MyForm::button3_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	openFileDialog(2);
+	return System::Void();
+}
+
+
---
+++ TestProject/MyForm.h
@@ -0,0 +1,133 @@
+#pragma once
+
+namespace TestProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// MyForm の概要
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ここにコンストラクター コードを追加します
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^  textBox1;
+	protected:
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Button^  button3;
+
+	private:
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(12, 32);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(253, 19);
+			this->textBox1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(294, 28);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"参照";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(161, 165);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"実行";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(12, 88);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(253, 19);
+			this->textBox2->TabIndex = 3;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(294, 84);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 4;
+			this->button3->Text = L"参照";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(406, 261);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e);
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e);
+			 private:System::Void openFileDialog(int index);
+	private: String^ getDeleteExtension(String^ str);
+	private: String^ getDeleteTail(String^ str);
+
+	private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e);
+};
+}
